363704ca456f838063b49668106c132aa4d08902
rdg1029 <wcle3456@gmail.com>
2022-08-25 20:21:09 +0900

    Add Notice.tsx : Add notice page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
+import Notice from './pages/Notice';
 import './App.css';
 
 const App: React.FC = () => {
@@ -8,6 +9,7 @@ const App: React.FC = () => {
         <div className="App">
             <Routes>
                 <Route path='/' element={<Main />} />
+                <Route path='/notice' element={<Notice />} />
             </Routes>
         </div>
     );
