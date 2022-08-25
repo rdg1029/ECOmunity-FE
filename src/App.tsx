fd609a4d4c74f7654c29ea09c16bd837594aea99
rdg1029 <wcle3456@gmail.com>
2022-08-25 20:27:52 +0900

    Update Profile.tsx : Add Profile page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+
 import Main from './pages/Main';
 import Notice from './pages/Notice';
 import Post from './pages/Post';
+import Profile from './pages/Profile';
+
 import './App.css';
 
 const App: React.FC = () => {
@@ -12,6 +15,7 @@ const App: React.FC = () => {
                 <Route path='/' element={<Main />} />
                 <Route path='/notice' element={<Notice />} />
                 <Route path='/post' element={<Post />} />
+                <Route path='/profile' element={<Profile />} />
             </Routes>
         </div>
     );
