bdd37f745922888c66f54e23a6439f87d534e58d
rdg1029 <wcle3456@gmail.com>
2022-08-25 20:08:12 +0900

    Change indent from 2 to 4

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,12 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
