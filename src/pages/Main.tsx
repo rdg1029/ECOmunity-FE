ab1005f7a663062141498a00733430c489b45036
Seungmin Jeong <seungmin1266@gmail.com>
2022-09-01 10:14:12 +0900

    Add MainStyle.tsx and test code

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import MainStyle from "../styles/MainStyle";
 
 const Main: React.FC = () => {
     return (
         <div>
             <h1>Main page</h1>
+            <MainStyle/>
+
             <p><Link to='/profile'>프로필</Link></p>
             <p><Link to='/login'>로그인</Link></p>
             <p><Link to='/notice'>공지</Link></p>
---
+++ src/styles/MainStyle.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const MainStyle = styled.div`
+    width: 100px;
+    height: 100px;
+    background-color: black;
+`;
+
+export default MainStyle;
