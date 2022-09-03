b2431eb2676b7134a71fbf187a0a16e0461285fe
rdg1029 <wcle3456@gmail.com>
2022-09-03 18:02:13 +0900

    Update Login : Apply request google login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React from 'react';
+import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import auth from '../auth'
 
 const Login: React.FC = () => {
+    function signInGoogle() {
+        signInWithPopup(auth, new GoogleAuthProvider()).then(result => {
+            console.log(result);
+        });
+    }
+
     return (
         <div>
             <h1>Login page</h1>
+            <button onClick={signInGoogle}>로그인</button>
         </div>
     );
 }
