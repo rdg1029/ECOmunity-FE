9d15be6aa07bf86fbfd9ca0f10828340721ee5ff
rdg1029 <wcle3456@gmail.com>
2022-09-04 14:26:23 +0900

    Update Login : Apply sign out

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,19 +1,42 @@
-import React from 'react';
-import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import React, { useState } from 'react';
+import { signInWithPopup, GoogleAuthProvider, signOut } from 'firebase/auth'
 import auth from '../auth'
 import user from '../data/User';
 
 const Login: React.FC = () => {
+    const [isSigned, setSigned] = useState<boolean>(user.isSigned);
+
     function signInGoogle() {
         signInWithPopup(auth, new GoogleAuthProvider()).then(userCredential => {
             user.setLogin(userCredential.user);
+            setSigned(user.isSigned);
+        });
+    }
+
+    function signOutGoogle() {
+        signOut(auth).then(() => {
+            user.setLogout();
+            setSigned(user.isSigned);
         });
     }
 
     return (
         <div>
-            <h1>Login page</h1>
-            <button onClick={signInGoogle}>로그인</button>
+            {isSigned ?
+            <div>
+                <h1>환영합니다!</h1>
+                <img src={user.photoURL as string}></img>
+                <p>{user.displayName}</p>
+                <p>{user.email}</p>
+                <button onClick={signOutGoogle}>로그아웃</button>
+            </div>
+            :
+            <div>
+                <h1>Login page</h1>
+                <button onClick={signInGoogle}>로그인</button>
+            </div>
+            }
+            
         </div>
     );
 }
