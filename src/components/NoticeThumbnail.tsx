9e054d20e1315b936900619d211df98c7488be82
Seungmin Jeong <seungmin1266@gmail.com>
2022-09-08 01:19:59 +0900

    [Updated components] : Add NoticeThumbnail.tsx

---
+++ src/components/NoticeThumbnail.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const NoticeThumbnail : React.FC = () => {
+    return(
+        <>
+        </>
+    );
+}
+
+export default NoticeThumbnail;
--- src/pages/Notice.tsx
+++ src/pages/Notice.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import NoticeDeco from "../components/NoticeDeco";
 
+const companyNum = 3;
+
+const NoticeText: React.FC = () => {
+    return(
+        <>
+        <h1>환경 보호 캠페인</h1>
+        <h1>{companyNum} 개의 기업이 함께하고 있어요. </h1>
+        </>
+    );
+}
+
 const Notice: React.FC = () => {
     return (
         <div>
             <NoticeDeco/>
             <h1>Notice page</h1>
+            <NoticeText/>
         </div>
     );
 }
