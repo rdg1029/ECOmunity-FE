ef1ce84556f5a899c5519f9bcb4db4dd8c7385f9
rdg1029 <wcle3456@gmail.com>
2022-09-08 22:49:34 +0900

    Update PostWrite : Apply on text change listener

--- src/components/PostWrite.tsx
+++ src/components/PostWrite.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Modal from "./Modal";
 
 interface Props {
@@ -11,15 +11,23 @@ const PostWrite: React.FC<Props> = (props) => {
     const [title, setTitle] = useState<string>();
     const [content, setContent] = useState<string>();
 
-    const imgElem = useRef<HTMLImageElement>(null);
-    const inputImg = useRef<HTMLInputElement>(null);
-
     const onImageChange: React.ChangeEventHandler<HTMLInputElement> = e => {
         const imgFile = e.currentTarget.files![0];
         const reader = new FileReader();
         reader.readAsDataURL(imgFile);
         reader.onload = e => {
             setImage(reader.result?.toString());
+        }
+    }
+
+    const onTextChange: React.ChangeEventHandler<HTMLInputElement> = e => {
+        switch(e.currentTarget.name) {
+            case "postTitle":
+                setTitle(e.currentTarget.value);
+                break;
+            case "postContent":
+                setContent(e.currentTarget.value);
+                break;
         }
     }
 
@@ -31,8 +39,8 @@ const PostWrite: React.FC<Props> = (props) => {
             }
             main={
                 <form style={{ display: "flex", flexDirection: "column"}}>
-                    <img src={image} ref={imgElem}></img>
-                    <label>사진 업로드 : <input ref={inputImg} type="file" accept="image/png, image/jpeg" name="postImage" onChange={onImageChange}></input></label> 
+                    <img src={image}></img>
+                    <label>사진 업로드 : <input type="file" accept="image/png, image/jpeg" name="postImage" onChange={onImageChange}></input></label> 
                     <input type="text" placeholder="제목" name="postTitle"></input>
                     <textarea placeholder="내용" name="postContent"></textarea>
                 </form>
