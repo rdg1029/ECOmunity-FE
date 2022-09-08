55d19144e991c7276e1662985d5ea9ac043e33b4
rdg1029 <wcle3456@gmail.com>
2022-09-08 18:51:21 +0900

    Update Post : Apply write post component

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import PostWrite from "../components/PostWrite";
 
 const Post: React.FC = () => {
+    const [isWriteMode, setWriteMode] = useState<boolean>(false);
+
     return (
         <div>
             <h1>Post page</h1>
+            <button onClick={() => setWriteMode(true)}>작성</button>
+            <PostWrite show={isWriteMode} onClose={() => setWriteMode(false)} onSubmit={() => console.log('submit')}/>
         </div>
     );
 }
