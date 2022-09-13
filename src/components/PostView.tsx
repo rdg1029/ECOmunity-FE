7d46a543b092e65e2d42d6bb8309514ddd66260a
rdg1029 <wcle3456@gmail.com>
2022-09-13 22:55:17 +0900

    Update PostView : Apply axios

--- src/components/PostView.tsx
+++ src/components/PostView.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import axios from "axios";
+import auth from "../auth"
 
 interface Props {
     id: string | null;
 }
 
 const PostView: React.FC<Props> = (props) => {
+    const [isLoaded, setLoaded] = useState<boolean>(false);
+    onAuthStateChanged(auth, user => {
+        if (!user) return;
+        user.getIdToken().then(idToken => {
+            axios.post('https://eco-api.defcon.or.kr/board/getPost', {
+                USER_UID: user.uid,
+                USER_TOKEN: idToken,
+                POST_ID: props.id,
+                POST_IS_NOTICE: false,
+            }).then(res => {
+                console.log(res);
+            }).catch(err => {
+                console.log(err);
+            });
+        }); 
+    });
     return (
         <div>
             <h1>Post ID : {props.id}</h1>
