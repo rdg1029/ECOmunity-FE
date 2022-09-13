8cfe6490d9cea5f26f433d603ec4d2ac22343e32
Seungmin Jeong <seungmin1266@gmail.com>
2022-09-14 01:18:42 +0900

    [Updated Login.tsx] : 있어빌리티 성공

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,6 +4,7 @@ import auth, {signInGoogle, signOutGoogle} from '../auth'
 import styled from 'styled-components';
 
 const Login: React.FC = () => {
+
     const [isLogin, setLogin] = useState<boolean>();
     onAuthStateChanged(auth, (user) => {
         if (user) {
@@ -15,30 +16,87 @@ const Login: React.FC = () => {
 
     return (
         <LoginMainStyle>
+            <LoginBgImageStyle src="https://images.pexels.com/photos/209982/pexels-photo-209982.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
             {isLogin ?
-            <div>
-                <h1>환영합니다!</h1>
-                <img src={auth.currentUser?.photoURL as string}></img>
-                <p>{auth.currentUser?.displayName}</p>
-                <p>{auth.currentUser?.email}</p>
-                <button onClick={signOutGoogle}>로그아웃</button>
-            </div>
-            :
-            <div>
-                <h1>Login page</h1>
-                <button onClick={signInGoogle}>로그인</button>
-            </div>
+                <LoginContentsStyle>
+                    <h1>환영합니다!</h1>
+                    <img src={auth.currentUser?.photoURL as string}></img>
+                    <p>안녕하세요, {auth.currentUser?.displayName}님</p>
+                    <p>오늘도 ECOmunity와 함께해주셔서 감사합니다</p>
+                    <button onClick={signOutGoogle}>로그아웃</button>
+                </LoginContentsStyle>
+                :
+                <LoginContentsStyle>
+                    <h1>안녕하세요</h1>
+                    <h2>우리의 행동으로 만드는 푸르른 발자취</h2>
+                    <h1>ECOmunity입니다.</h1>
+                    <button onClick={signInGoogle}>Google 계정으로 로그인</button>
+                </LoginContentsStyle>
             }
-            
-        </LoginMainStyle>
+            </LoginMainStyle>
     );
 }
+
+const LoginContentsStyle = styled.div`
+    @import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.5/dist/web/static/pretendard.css");
+    font-family: 'Pretendard-Thin';
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 180px;
+    font-size: 20pt;
+    color: #FFF;
+
+
+    h1{
+        font-family: 'Pretendard-Bold';
+        font-size: 50pt;
+    }
+    img{
+        width:50%;
+        border-radius: 70%;
+        border: 5px solid #00AE68;
+    }
+    button{
+        background: #00AE68;
+        position: relative;
+        display: block;
+        width: 40%;
+        padding: 0;
+        margin: 10px 20px 10px 0;
+        font-weight: 600;
+        text-align: center;
+        line-height: 50px;
+        color: #FFF;
+        border-radius: 5px;
+        transition: all 0.2s;
+        justify-content: center;
+        align-items: center;
+        float: left;
+        font-size: 20px;
+    }
+    button:hover{
+        background: #21825B;
+    }
+    
+`;
 
 const LoginMainStyle = styled.div`
     width: 100%;
     height: 100%;
-    background-color: red;
-    margin-top: 200px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const LoginBgImageStyle = styled.img`    
+    width: 100%;
+    height: 150%;
+    position: fixed;
+    z-index: -100;
+    background-color: rgba(0,0,128,0.1);
+    filter: blur(10px);
 `;
 
 export default Login;
