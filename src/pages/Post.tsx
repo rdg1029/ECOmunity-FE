aec4ef11c6f0e48be94a95bc0ea374d82f338a48
Seungmin Jeong <seungmin1266@gmail.com>
2022-09-14 22:25:14 +0900

    [Updated Post.tsx] : Add button styles

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import auth from "../auth";
 import PostWrite from "../components/PostWrite";
 
@@ -6,12 +7,43 @@ const Post: React.FC = () => {
     const [isWriteMode, setWriteMode] = useState<boolean>(false);
 
     return (
-        <div>
-            <h1>Post page</h1>
+        <PostGlobalStyle>
+            <h1>ECOmunity 활동 인증</h1>
             <button onClick={() => auth.currentUser ? setWriteMode(true) : window.alert('로그인이 필요한 서비스입니다.')}>작성</button>
             <PostWrite show={isWriteMode}/>
-        </div>
+        </PostGlobalStyle>
     );
 }
+
+const PostGlobalStyle = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    
+    button{
+        background: #00AE68;
+        position: relative;
+        display: block;
+        width: 120px;
+        padding: 0;
+        margin: 10px 20px 10px 0;
+        font-weight: 600;
+        text-align: center;
+        line-height: 50px;
+        color: #FFF;
+        border-radius: 5px;
+        transition: all 0.2s;
+        justify-content: center;
+        align-items: center;
+        float: left;
+        font-size: 20px;
+    }
+    button:hover{
+        background: #21825B;
+    }
+`
 
 export default Post;
