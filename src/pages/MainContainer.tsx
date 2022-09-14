a83238637c7894c4e8e30a8dabcb8795cb6d490e
rdg1029 <wcle3456@gmail.com>
2022-09-14 17:09:55 +0900

    Update MainContainer : Remove global main style

--- src/pages/MainContainer.tsx
+++ src/pages/MainContainer.tsx
@@ -5,33 +5,23 @@ import MyPoint from "../components/MyPoint";
 
 const MainContainer : React.FC = () =>{
     return(
-        <GlobalMainStyle>
-            <MainLayoutContainer>
-                <BackgroundImgStyle>
-                    <img src="https://img.freepik.com/free-photo/beautiful-tree-in-the-middle-of-a-field-covered-with-grass-with-the-tree-line-in-the-background_181624-29267.jpg?w=2000&t=st=1662966110~exp=1662966710~hmac=6a8862695102a5f4d119da4a27870bf21c4975118d2a87d3f6c38ec77e64cd5e"/>
-                </BackgroundImgStyle>
-                <Title/>
-                <MainLayoutPointStyle>
-                    <MyPoint/>
-                </MainLayoutPointStyle>
-            </MainLayoutContainer>
-        </GlobalMainStyle>
-        
+        <MainLayoutContainer>
+            <BackgroundImgStyle src="https://img.freepik.com/free-photo/beautiful-tree-in-the-middle-of-a-field-covered-with-grass-with-the-tree-line-in-the-background_181624-29267.jpg?w=2000&t=st=1662966110~exp=1662966710~hmac=6a8862695102a5f4d119da4a27870bf21c4975118d2a87d3f6c38ec77e64cd5e"/>
+            <Title/>
+            <MainLayoutPointStyle>
+                <MyPoint/>
+            </MainLayoutPointStyle>
+        </MainLayoutContainer>
     );
 }
-
-const GlobalMainStyle = styled.div`
-    height: 100%;
-    width: 100%;
-`;
 /**전역 배경 색상 스타일 */
 
-const BackgroundImgStyle = styled.div`
+const BackgroundImgStyle = styled.img`
     width: 100%;
-    height: 100%;
+    height: 150%;
     position: fixed;
-    z-index: -10;
-    
+    z-index: -100;
+    background-color: rgba(0,0,128,0.1);
 `;
 
 const MainLayoutContainer = styled.div`
@@ -40,7 +30,6 @@ const MainLayoutContainer = styled.div`
     justify-content: center;
     width: 100%;
     position: absolute;
-    z-index: 50;
 `;
 /**메인 레이아웃 컨테이너*/
 
