30e7b0d899a00a218c962ed3bf8605240d422af5
rdg1029 <wcle3456@gmail.com>
2022-09-14 22:24:53 +0900

    Update PostView : Add getPost()

--- src/components/PostView.tsx
+++ src/components/PostView.tsx
@@ -8,13 +8,34 @@ interface Props {
     id: string | null;
 }
 
+interface API_POST_DATA {
+    POST_AUTHOR: string,
+    POST_DATE: string,
+    POST_CONTENT: string,
+    POST_IMAGE: string,
+    POST_TITLE: string
+}
+
 const PostViewLayout = styled.div`
     display: flex;
     flex-direction: column;
 `;
 
+function getPost(data: any) {
+    return (
+        <div>
+            <h1>{data.POST_TITLE}</h1>
+            <p>{data.POST_AUTHOR}</p>
+            <p>{data.POST_DATE}</p>
+            <img src={data.POST_IMAGE}/>
+            <p>{data.POST_CONTENT}</p>
+        </div>
+    )
+}
+
 const PostView: React.FC<Props> = (props) => {
     const [isLoaded, setLoaded] = useState<boolean | null>(false);
+    const [postData, setPostData] = useState<any>();
     onAuthStateChanged(auth, user => {
         if (!user) return;
         user.getIdToken().then(idToken => {
@@ -26,6 +47,7 @@ const PostView: React.FC<Props> = (props) => {
             }).then(res => {
                 console.log(res);
                 setLoaded(true);
+                setPostData(res);
             }).catch(err => {
                 console.log(err);
                 setLoaded(null);
@@ -36,7 +58,7 @@ const PostView: React.FC<Props> = (props) => {
         <PostViewLayout>
             {
                 isLoaded ?
-                <h1>내용 불러옴</h1>
+                getPost(postData)
                 :
                 isLoaded === false ?
                 <h1>불러오는 중...</h1>
