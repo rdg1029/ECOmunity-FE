b39fde502dd97030d225886edca9b35a5b751471
rdg1029 <wcle3456@gmail.com>
2022-09-14 17:06:00 +0900

    Update NavBase : Remove Outlet

--- src/components/NavBase.tsx
+++ src/components/NavBase.tsx
@@ -1,23 +1,19 @@
 import React from "react";
 import styled from "styled-components";
-import { Outlet } from "react-router-dom";
 import Nav from "./Nav";
 
-const NavBase = () => {
-    return(
-            <NavContainer>
-                <Nav/>
-                <Outlet/>
-            </NavContainer>
-                
+const NavBase: React.FC = () => {
+    return (
+        <NavContainer>
+            <Nav/>
+        </NavContainer>
     )
 }
 
 const NavContainer = styled.div`
     width: 100%;
-    position: fixed;
+    // position: fixed;
     z-index: 300;
 `;
-
 
 export default NavBase;
