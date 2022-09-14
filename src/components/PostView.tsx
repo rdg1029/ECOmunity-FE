6ef6f76ea3efe3563d47e3d3219d88a1c8903736
rdg1029 <wcle3456@gmail.com>
2022-09-14 17:04:31 +0900

    Update PostView : Apply post view layout

--- src/components/PostView.tsx
+++ src/components/PostView.tsx
@@ -2,13 +2,19 @@ import React, { useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import axios from "axios";
 import auth from "../auth"
+import styled from "styled-components";
 
 interface Props {
     id: string | null;
 }
 
+const PostViewLayout = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
 const PostView: React.FC<Props> = (props) => {
-    const [isLoaded, setLoaded] = useState<boolean>(false);
+    const [isLoaded, setLoaded] = useState<boolean | null>(false);
     onAuthStateChanged(auth, user => {
         if (!user) return;
         user.getIdToken().then(idToken => {
@@ -19,15 +25,25 @@ const PostView: React.FC<Props> = (props) => {
                 POST_IS_NOTICE: false,
             }).then(res => {
                 console.log(res);
+                setLoaded(true);
             }).catch(err => {
                 console.log(err);
+                setLoaded(null);
             });
         }); 
     });
     return (
-        <div>
-            <h1>Post ID : {props.id}</h1>
-        </div>
+        <PostViewLayout>
+            {
+                isLoaded ?
+                <h1>내용 불러옴</h1>
+                :
+                isLoaded === false ?
+                <h1>불러오는 중...</h1>
+                :
+                <h1>게시글을 불러올 수 없습니다!</h1>
+            }
+        </PostViewLayout>
     )
 }
 export default PostView;
