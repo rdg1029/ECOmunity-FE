bf03cb4b4553bfba40aee7c224151c6e5448113f
Seungmin Jeong <seungmin1266@gmail.com>
2022-09-15 14:40:04 +0900

    [Updated PostListLayout.tsx] : Add Deco

--- src/components/PostListLayout.tsx
+++ src/components/PostListLayout.tsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
+import auth from "../auth";
 import PostTitle from "./PostTitle";
 import PostThumbnail from "./PostThumbnail";
-
-
+import PostWrite from "./PostWrite";
 
 const PostListLayout : React.FC = () => {
+
+    const [isWriteMode, setWriteMode] = useState<boolean>(false);
     return(
         <PostLayoutGlobalStyle>
-            <PostTitle/> {/**타이틀 컴포 */}
+            <TitleAndButtonStyle>
+                <PostTitle/> {/**타이틀 컴포 */}
+                <button onClick={() => auth.currentUser ? setWriteMode(true) : window.alert('로그인이 필요한 서비스입니다.')}>내 발자취 남기기</button>
+            </TitleAndButtonStyle>
+            <PostWrite show={isWriteMode}/>
                 <PostThumbnailStyle>
                     <PostThumbnail/>{/**썸넬 6개 넣어야 함. */}
                     <PostThumbnail/>
@@ -31,11 +37,36 @@ const PostLayoutGlobalStyle = styled.div`
     margin-top: 40px;
     justify-content: flex-start;
     align-items: flex-start;
-;
-    
 `;
-/**공지사항 컴포넌트 전체에 대한 스타일 */
 
+const TitleAndButtonStyle = styled.div`
+    display: flex;
+    flex-direction: row;
+
+    button{
+        background: #00AE68;
+        position: relative;
+        display: block;
+        width: 200px;
+        height: 50px;
+        padding: 0;
+        margin: 30px 20px 10px 900px;
+        font-weight: 600;
+        text-align: center;
+        line-height: 50px;
+        color: #FFF;
+        border-radius: 5px;
+        transition: all 0.2s;
+        justify-content: center;
+        align-items: center;
+        float: left;
+        font-size: 20px;
+        border: 2px solid #21825B
+    }
+    button:hover{
+        background: #21825B;
+    }
+`
 
 
 const PostThumbnailStyle = styled.div`
--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,18 +1,12 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import auth from "../auth";
-import PostWrite from "../components/PostWrite";
 import PostListLayout from "../components/PostListLayout";
 
 const Post: React.FC = () => {
-    const [isWriteMode, setWriteMode] = useState<boolean>(false);
 
     return (
         <PostGlobalStyle>
-            <h1>나의 발자취 공유하기</h1>
-            <button onClick={() => auth.currentUser ? setWriteMode(true) : window.alert('로그인이 필요한 서비스입니다.')}>작성</button>
-            <PostWrite show={isWriteMode}/>
-
+            <PostBgImgStyle src="https://cdn.pixabay.com/photo/2016/06/27/17/54/leaf-1482948_960_720.jpg"/>
             <PostListLayout/>
         </PostGlobalStyle>
     );
@@ -25,28 +19,19 @@ const PostGlobalStyle = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    
-    button{
-        background: #00AE68;
-        position: relative;
-        display: block;
-        width: 120px;
-        padding: 0;
-        margin: 10px 20px 10px 0;
-        font-weight: 600;
-        text-align: center;
-        line-height: 50px;
-        color: #FFF;
-        border-radius: 5px;
-        transition: all 0.2s;
-        justify-content: center;
-        align-items: center;
-        float: left;
-        font-size: 20px;
-    }
-    button:hover{
-        background: #21825B;
-    }
-`
+`;
+
+const PostBgImgStyle = styled.img`
+    width: 2400px;
+    height: 1600px;
+    border-radius: 120px;
+    margin-top: -500px;
+    margin-left: 1000px;
+    position: fixed;
+    z-index: -100;
+    filter: blur(3px);
+    transform: rotate(-40deg);
+`;
+
 
 export default Post;
