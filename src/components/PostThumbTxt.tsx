d9e107596ddc2c91b883fa3d8f810a492246cc97
Seungmin Jeong <seungmin1266@gmail.com>
2022-09-15 14:21:34 +0900

    [Updated PostThumbTxt.tsx] : Add Like icon

--- src/components/PostThumbTxt.tsx
+++ src/components/PostThumbTxt.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLeaf } from '@fortawesome/free-solid-svg-icons'
+
+const leafCnt = 123;
+
 
 const PostThumbTxt : React.FC = () => {
     return(
@@ -18,15 +23,28 @@ const PostThumbTxt : React.FC = () => {
                     모든 국민은 행위시의 법률에 의하여 범죄를 구성하지 아니하는 행위로 소추되지 아니하며, 동일한 범죄에 대하여 거듭 처벌
                 </p>
             </PostPreviewStyle>
+            
+            <OtherContetnsStyle>
+                <LikeStyle>
+                    <FontAwesomeIcon icon={faLeaf} color="#14C38E" size="2x"/>
+                    <p>{leafCnt}</p>
+                </LikeStyle>
 
-            <WriterDateStyle>
-                <p>김 현 우</p>
-                <p>22.09.15</p>
-            </WriterDateStyle>
+                <WriterDateStyle>
+                    <p>김 현 우</p>
+                    <p>22.09.15</p>
+                </WriterDateStyle>
+            </OtherContetnsStyle>
             
         </PostTxtGlobalStyle>
     );
 };
+
+const OtherContetnsStyle = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
 
 const PostPreviewStyle = styled.div`
     width: 90%;
@@ -34,6 +52,22 @@ const PostPreviewStyle = styled.div`
     margin-left: 23px;
     
 `;
+
+const LikeStyle = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    margin-left: 20px;
+    margin-top: 0px;
+
+    p{
+        font-size: 15pt;
+        margin-left: 10px;
+    }
+
+`;
+
 
 const WriterDateStyle = styled.div`
     @import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.5/dist/web/static/pretendard.css");
@@ -44,8 +78,8 @@ const WriterDateStyle = styled.div`
     flex-direction: column;
     justify-content: flex-end;
     align-items: flex-end;
-    margin: 0 0 0 340px;
-    padding-bottom: 10px;
+    margin: 45px 0 0 260px;
+    padding-bottom: 40px;
 
     p{
         margin: 0;
