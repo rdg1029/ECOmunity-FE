0bda3292cd424273c836cb8fd1ffc44fb6412a45
Seungmin Jeong <seungmin1266@gmail.com>
2022-09-16 09:00:43 +0900

    [Updated MyPoint.tsx] : Deleted 사용처 보기 and  adjustment margin

--- src/components/MyPoint.tsx
+++ src/components/MyPoint.tsx
@@ -4,7 +4,35 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import auth from "../auth";
 
+const MyPoint : React.FC = () => {
+    const [isLogin, setLogin] = useState<boolean>();
+    const user = auth.currentUser?.providerData[0];
+    const usrName = user?.displayName;
 
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            setLogin(true);
+            return;
+        }
+        setLogin(false);
+    });
+    return(
+        <>
+            <MyPointLayoutStyle>
+                <MyPointTextLayoutStyle>
+                    {isLogin ?
+                    <>
+                        <p>{usrName} 님의 Ecomunity 포인트</p>
+                            <h1> 0 점 </h1>
+                    </>
+                    :
+                    <p>안녕하세요 이커뮤니티입니다.<br/> 로그인을 해주세요.</p>
+                    }
+                </MyPointTextLayoutStyle>
+            </MyPointLayoutStyle>
+        </>
+    );
+};
 
 const MyPointLayoutStyle = styled.div`
     display: flex;
@@ -12,6 +40,7 @@ const MyPointLayoutStyle = styled.div`
     align-items: flex-start;
     color : white;
     height: 280px;
+    margin-top: 60px;
     margin-left: 300px;
 
     
@@ -33,38 +62,7 @@ const MyPointTextLayoutStyle = styled.div`
         @import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.5/dist/web/static/pretendard.css");
         font-family: 'Pretendard-Bold';
         font-size: 80px;
+        
     }
 `;
-
-const MyPoint : React.FC = () => {
-    const [isLogin, setLogin] = useState<boolean>();
-    const user = auth.currentUser?.providerData[0];
-    const usrName = user?.displayName;
-
-    onAuthStateChanged(auth, (user) => {
-        if (user) {
-            setLogin(true);
-            return;
-        }
-        setLogin(false);
-    });
-    return(
-        <>
-            <MyPointLayoutStyle>
-                <MyPointTextLayoutStyle>
-                    {isLogin ?
-                    <>
-                        <p>{usrName} 님의 Ecomunity 포인트</p>
-                            <h1> 0 점 </h1>
-                        <p>{'>'} Ecomunity 포인트 사용처 보기</p>
-                    </>
-                    :
-                    <p>안녕하세요 이커뮤니티입니다.<br/> 로그인을 해주세요.</p>
-                    }
-                </MyPointTextLayoutStyle>
-            </MyPointLayoutStyle>
-        </>
-    );
-}
-
 export default MyPoint;
