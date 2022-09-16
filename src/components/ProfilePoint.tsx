7516af7d50acd882a58cbb653f2edb7c54d57270
rdg1029 <wcle3456@gmail.com>
2022-09-16 11:14:14 +0900

    Update ProfilePoint : Get point from api server

--- src/components/ProfilePoint.tsx
+++ src/components/ProfilePoint.tsx
@@ -2,12 +2,26 @@ import React, {useState} from "react";
 import { onAuthStateChanged } from 'firebase/auth';
 import auth from "../auth";
 import styled from "styled-components";
+import request from "../util/RequestAPI";
 
 const ProfilePoint:React.FC = () => {
     const [point, setPoint] = useState(0);
     const [isLogin, setLogin] = useState<boolean>();
     onAuthStateChanged(auth, (user) => {
         if (user) {
+            user?.getIdToken().then(token => {
+                request.post('/profile/getUserInfo', {
+                    USER_UID: user.uid,
+                    USER_TOKEN: token,
+                })
+                .then(res => {
+                    setLogin(true);
+                    setPoint(res.data.RESULT_DATA.USER_POINT);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+            });
             setLogin(true);
             return;
         }
@@ -22,7 +36,7 @@ const ProfilePoint:React.FC = () => {
                 <ProfileUserPointStyle>{point} 점</ProfileUserPointStyle>
             </div>
             :
-            <h1>로그인부터 하세욤</h1>
+            <h1>로그인 하고 나의 점수를 확인해보세요.</h1>
             }
         </div>
     );
